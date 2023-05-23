5a064ed3adc6932d618027d5684f7502372fef78
putri <@students.uin-suska.ac.id>
2023-05-23 08:40:04 +0700

    Navbar Img

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary" style="font-family: Avenir, Helvetica, Arial, sans-serif">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">Al-Qur'an Indonesia</a>
       <div class="collapse navbar-collapse" id="navbarNav">
-
             <a class="nav-link" href="/surah">Daftar Surah</a>
-
-
       </div>
     </div>
   </nav>
